c0546c0242960cd41504bfa25fbaedbe17e97298
Gerard Bentley <gerardrbentley@gmail.com>
2022-06-30 17:52:07 -0700

    change: no cross validation too slow in cloud

--- pages/04_Decision_Trees.py
+++ pages/04_Decision_Trees.py
@@ -32,8 +32,67 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.inspection import permutation_importance
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_validate, train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+
+
+def render_end():
+    st.write(
+        """## Take it further:
+
+- Compare results to simpler tabular models (Regression, Random Forest) or other Gradient Boosting implementations (XGBoost, LightGBM, CatBoost)
+- Perform a 'regression' task instead of 'classification' to get a pseudo-confidence score from the model
+- Utilize Cross-Validation with Quantile Losses or Mean losses to assess confidence intervals
+- Explore tabular models in other use cases such as Time Series analysis
+    """
+    )
+
+    if st.checkbox("Show Code (~200 lines)"):
+        with open(__file__, "r") as f:
+            st.code(f.read())
+    st.stop()
+
+
+@st.experimental_memo
+def run_training(features, labels):
+    model = HistGradientBoostingClassifier(
+        random_state=47, categorical_features=categorical_column_mask
+    )
+    features_train, features_test, labels_train, labels_test = train_test_split(
+        features, labels, random_state=47
+    )
+    model.fit(features_train, labels_train)
+    labels_predicted = model.predict(features_test)
+    labels_predicted_train = model.predict(features_train)
+    test_score = accuracy_score(labels_predicted, labels_test)
+    train_score = accuracy_score(labels_predicted_train, labels_train)
+    feature_importance = get_importances(model, features_train, labels_train)
+
+    labels_predicted_display = species_encoder.inverse_transform(labels_predicted)
+    labels_test_display = species_encoder.inverse_transform(labels_test)
+    results = pd.DataFrame(
+        {
+            "Predicted Species": labels_predicted_display,
+            "Actual Species": labels_test_display,
+            **features_test[feature_cols],
+        }
+    )
+    return model, results, feature_importance, train_score, test_score
+
+
+def get_importances(model, features, labels):
+    result = permutation_importance(
+        model, features, labels, n_repeats=10, random_state=47
+    )
+
+    sorted_importances_idx = result.importances_mean.argsort()
+    importances = pd.DataFrame(
+        result.importances[sorted_importances_idx].T,
+        columns=features.columns[sorted_importances_idx],
+    )
+    return importances
+
 
 penguin_df = pd.read_csv(
     "data/penguins.csv",
@@ -83,102 +142,29 @@ features["island"] = island_encoder.fit_transform(penguin_df["island"])
 features["sex"] = sex_encoder.fit_transform(penguin_df["sex"])
 
 
-@st.experimental_singleton
-def get_model():
-    model = HistGradientBoostingClassifier(
-        random_state=47, categorical_features=categorical_column_mask
-    )
-    return model
-
-
-def get_importances(model, features, labels):
-    result = permutation_importance(
-        model, features, labels, n_repeats=10, random_state=47
-    )
-
-    sorted_importances_idx = result.importances_mean.argsort()
-    importances = pd.DataFrame(
-        result.importances[sorted_importances_idx].T,
-        columns=features.columns[sorted_importances_idx],
-    )
-    return importances
-
-
-@st.experimental_memo
-def run_cross(features, labels):
-    model = get_model()
-    results = cross_validate(
-        model,
-        features,
-        labels,
-        cv=5,
-        scoring="accuracy",
-        return_train_score=True,
-        return_estimator=True,
-    )
-    importances = [
-        get_importances(cv_model, features, labels) for cv_model in results["estimator"]
-    ]
-    results["feature_importance"] = importances
-    return results
-
-
-def render_end():
-    st.write(
-        """## Take it further:
-
-    - Compare results to simpler tabular models (Regression, Random Forest) or other Gradient Boosting implementations (XGBoost, LightGBM, CatBoost)
-    - Perform a 'regression' task instead of 'classification' to get a pseudo-confidence score from the model
-    - Utilize Quantile Losses instead of Mean losses to assess confidence intervals
-    - Explore tabular models in other use cases such as Time Series analysis
-    """
-    )
-
-    if st.checkbox("Show Code (~190 lines)"):
-        with open(__file__, "r") as f:
-            st.code(f.read())
-    st.stop()
-
-
-if not st.checkbox("Press Here to Run K-Fold Cross Validation"):
+if not st.checkbox("Press Here to Train Model on 75% of the data"):
     render_end()
-results = run_cross(features, labels)
-st.header("Accuracy scores over 5-fold Cross Validation")
-trained_models = results["estimator"]
-training_scores = results["train_score"]
-testing_scores = results["test_score"]
-feature_importances = results["feature_importance"]
-for i, (model, train_score, test_score, feature_importance) in enumerate(
-    zip(trained_models, training_scores, testing_scores, feature_importances)
-):
-    st.subheader(f"Fold {i + 1} Results")
-    st.write(f"{train_score = }, {test_score = }")
-    predictions = model.predict(features)
-    prediction_labels = species_encoder.inverse_transform(predictions)
-    results = pd.DataFrame(
-        {
-            "Predicted Species": prediction_labels,
-            "Actual Species": penguin_df["species"],
-            **penguin_df[feature_cols],
-        }
-    )
-    with st.expander("Missed Predictions"):
-        results[results["Predicted Species"] != results["Actual Species"]]
 
-    if st.checkbox("Show Feature Importance", key=str(i)):
-        fig, ax = plt.subplots()
-        feature_importance.plot.box(vert=False, whis=10, ax=ax)
-        ax.set_title("Permutation Importances")
-        ax.axvline(x=0, color="k", linestyle="--")
-        ax.set_xlabel("Decrease in accuracy score")
-        ax.figure.tight_layout()
-        st.pyplot(fig)
+model, results, feature_importance, train_score, test_score = run_training(
+    features, labels
+)
+with st.expander("Missed Predictions"):
+    results[results["Predicted Species"] != results["Actual Species"]]
 
-    st.download_button(
-        f"Download Trained Model {i + 1}",
-        pickle.dumps(model),
-        help="Download the model weights to be used for future predictions",
-    )
+if st.checkbox("Show Feature Importance", key="feat_importance"):
+    fig, ax = plt.subplots()
+    feature_importance.plot.box(vert=False, whis=10, ax=ax)
+    ax.set_title("Permutation Importances")
+    ax.axvline(x=0, color="k", linestyle="--")
+    ax.set_xlabel("Decrease in accuracy score")
+    ax.figure.tight_layout()
+    st.pyplot(fig)
+
+st.download_button(
+    f"Download Trained Model",
+    pickle.dumps(model),
+    help="Download the model weights to be used for future predictions",
+)
 
 st.header("Predict your own Penguin's species 🐧")
 
@@ -211,6 +197,6 @@ else:
     with st.expander("Raw Prediction"):
         prediction[0]
     (species,) = species_encoder.inverse_transform([prediction])
-    st.write(f"Most likely an **{species}** Penguin!")
+    st.success(f"Most likely an **{species}** Penguin!")
 
 render_end()
